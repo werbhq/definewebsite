bf568d290dada25642c2fec821f9352e86ed87f1
M Aswin Kishore <60577077+mak626@users.noreply.github.com>
2022-09-24 22:41:51 +0530

    fix: scrollbar

--- src/define/define.js
+++ src/define/define.js
@@ -1,36 +1,36 @@
-import Header from "./../header/header.js";
-import "./define.css";
-import DefineHero from "./hero/definehero.js";
-import DefineAbout from "./about/about";
-import DefineTheme from "./theme/theme.js";
-import Navbar from "./../components/navbar/navbar.js";
-import React, { useState, createContext } from "react";
-import DefineSponsors from "./sponsors/sponsors.js";
+import Header from './../header/header.js';
+import './define.css';
+import DefineHero from './hero/definehero.js';
+import DefineAbout from './about/about';
+import DefineTheme from './theme/theme.js';
+import Navbar from './../components/navbar/navbar.js';
+import React, { useState, createContext } from 'react';
+import DefineSponsors from './sponsors/sponsors.js';
 
 const BackgroundContext = createContext();
 const NavbarContext = createContext();
 
 function Define() {
-	const [background, setBackground] = useState("blackBG");
-	const [active, setActive] = useState(false);
-	return (
-		<NavbarContext.Provider value={{ active, setActive }}>
-			<BackgroundContext.Provider value={{ background, setBackground }}>
-				<div className={"Define " + background}>
-					<Header></Header>
-					<Navbar></Navbar>
-					<div className="plasticTexture"></div>
-					{/* <div className="glitchTexture"></div> */}
-					<div className="plainNoise"></div>
-					<DefineHero></DefineHero>
-					<DefineAbout></DefineAbout>
-					<DefineTheme></DefineTheme>
-					<DefineSponsors></DefineSponsors>
-					{/* <HUIButton text="Coming Soon" icon="go"></HUIButton> */}
-				</div>
-			</BackgroundContext.Provider>
-		</NavbarContext.Provider>
-	);
+    const [background, setBackground] = useState('blackBG');
+    const [active, setActive] = useState(false);
+    return (
+        <NavbarContext.Provider value={{ active, setActive }}>
+            <BackgroundContext.Provider value={{ background, setBackground }}>
+                <div className={'Define ' + background}>
+                    <Header></Header>
+                    <Navbar></Navbar>
+                    <div className="plasticTexture"></div>
+                    {/* <div className="glitchTexture"></div> */}
+                    <div className="plainNoise"></div>
+                    <DefineHero></DefineHero>
+                    <DefineAbout></DefineAbout>
+                    <DefineTheme></DefineTheme>
+                    <DefineSponsors></DefineSponsors>
+                    {/* <HUIButton text="Coming Soon" icon="go"></HUIButton> */}
+                </div>
+            </BackgroundContext.Provider>
+        </NavbarContext.Provider>
+    );
 }
 
 export { Define, BackgroundContext, NavbarContext };
--- src/define/hero/definehero.js
+++ src/define/hero/definehero.js
@@ -1,60 +1,54 @@
-import HUIButton from "../../components/button/button";
-import "./../hero/definehero.css";
-import LightBulbImage from "./../../assets/images/defineLightBulbVideo.gif";
-import { BackgroundContext } from "../define";
-import { useContext, useRef, useState } from "react";
-import { useInView } from "react-intersection-observer";
+import HUIButton from '../../components/button/button';
+import './../hero/definehero.css';
+import LightBulbImage from './../../assets/images/defineLightBulbVideo.gif';
+import { BackgroundContext } from '../define';
+import { useContext, useRef, useState } from 'react';
+import { useInView } from 'react-intersection-observer';
 
 function DefineHero() {
-	const { ref, inView, entry } = useInView({
-		/* Optional options */
-		threshold: 0.6,
-	});
+    const { ref, inView, entry } = useInView({
+        /* Optional options */
+        threshold: 0.6,
+    });
 
-	const { background, setBackground } = useContext(BackgroundContext);
-	const [shrink, setShrink] = useState(false);
+    const { setBackground } = useContext(BackgroundContext);
+    const [shrink, setShrink] = useState(false);
 
-	if (inView) {
-		setBackground("blackBG");
-	}
+    if (inView) {
+        setBackground('blackBG');
+    }
 
-	return (
-		<div className="hero" ref={ref}>
-			<div className="heroSectionA">
-				{/* <h6 className="titlesponsor">Title Sponsor Name</h6> */}
-				<h1 className="heroheader">define.</h1>
-				<h3 className="tagline">
-					<span>build for the</span>
-					<span className="youthfulYellowFG">
-						<br /> real world.
-					</span>
-				</h3>
+    return (
+        <div className="hero" ref={ref}>
+            <div className="heroSectionA">
+                {/* <h6 className="titlesponsor">Title Sponsor Name</h6> */}
+                <h1 className="heroheader">define.</h1>
+                <h3 className="tagline">
+                    <span>build for the</span>
+                    <span className="youthfulYellowFG">
+                        <br /> real world.
+                    </span>
+                </h3>
 
-				<br />
-				<div className="buttonContainer">
-					<HUIButton
-						text="Coming Soon"
-						icon="go"
-						event="define"
-						type="primary"
-						variant="1"
-						color="var(--blasphemous-black)"
-					></HUIButton>
-					<br />
-					<HUIButton
-						text="Stay Tuned"
-						icon="mail"
-						event="define"
-						type="secondary"
-						color="var(--youthful-yellow)"
-					></HUIButton>
-				</div>
-			</div>
-			<div className="heroSectionB">
-				<img src={LightBulbImage} alt="" />
-			</div>
-		</div>
-	);
+                <br />
+                <div className="buttonContainer">
+                    <HUIButton
+                        text="Coming Soon"
+                        icon="go"
+                        event="define"
+                        type="primary"
+                        variant="1"
+                        color="var(--blasphemous-black)"
+                    ></HUIButton>
+                    <br />
+                    <HUIButton text="Stay Tuned" icon="mail" event="define" type="secondary" color="var(--youthful-yellow)"></HUIButton>
+                </div>
+            </div>
+            <div className="heroSectionB">
+                <img src={LightBulbImage} alt="" />
+            </div>
+        </div>
+    );
 }
 
 export default DefineHero;
