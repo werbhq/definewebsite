82d722133d02768198df71d0d3d6f5d8ada33c73
Navaneeth Venu <nauaneeth@gmail.com>
2022-09-25 18:04:35 +0530

    Add construction page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import { Define } from "./define/define";
+import Construction from './construction/construction';
+import { Define } from './define/define';
+import Header from './header/header';
+import Hero from './hero/hero';
 
 function App() {
-  return (
-    <div className="App">
-      <Define></Define>
-    </div>
-  );
+    return (
+        <div className="App">
+            {/* <Header></Header> */}
+            {/* <Hero></Hero> */}
+            <Construction></Construction>
+            {/* <Define></Define> */}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/construction/construction.js
@@ -0,0 +1,14 @@
+import './construction.css';
+
+function Construction() {
+    return (
+        <div className="construction">
+            <h2 style={{ fontWeight: 200 }}>What's up?</h2>
+            <h2 style={{ color: 'var(--pretty-purple)' }}>#isHappening</h2>
+            <br />
+            <h3 style={{ letterSpacing: '0px', fontWeight: 400 }}>Stay tuned.</h3>
+        </div>
+    );
+}
+
+export default Construction;
