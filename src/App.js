7e757dd07167e507b2e09d8d67751440ad046690
Navaneeth Venu <nauaneeth@gmail.com>
2022-10-01 11:20:17 +0530

    Revert "Add construction page"
    This reverts commit 82d722133d02768198df71d0d3d6f5d8ada33c73.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import Construction from './construction/construction';
-import { Define } from './define/define';
-import Header from './header/header';
-import Hero from './hero/hero';
+import { Define } from "./define/define";
 
 function App() {
-    return (
-        <div className="App">
-            {/* <Header></Header> */}
-            {/* <Hero></Hero> */}
-            <Construction></Construction>
-            {/* <Define></Define> */}
-        </div>
-    );
+  return (
+    <div className="App">
+      <Define></Define>
+    </div>
+  );
 }
 
 export default App;
