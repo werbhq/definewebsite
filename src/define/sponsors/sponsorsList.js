7f33e66a419ceff190963d387c672563963aa4be
Navaneeth Venu <nauaneeth@gmail.com>
2022-10-01 11:54:33 +0530

    Revert "Revert "Add construction page"" and Add Sponsors
    This reverts commit 7e757dd07167e507b2e09d8d67751440ad046690.

--- src/define/sponsors/sponsorsList.js
+++ src/define/sponsors/sponsorsList.js
@@ -2,8 +2,13 @@ import DefineSeperator from '../../components/seperator/seperator';
 import SponsorItem from './sponsorItem';
 
 var data = [
-    { imageKey: 'Wolfram', imagePath: 'Wolfram.svg', URL: 'https://www.wolfram.com/', type: 'gold' },
+    { imageKey: 'Devfolio', imagePath: 'Devfolio.svg', URL: 'https://www.devfolio.com/', type: 'platinum' },
+    { imageKey: 'Polygon', imagePath: 'Polygon.svg', URL: 'https://polygon.technology/', type: 'platinum' },
     { imageKey: 'GitHub', imagePath: 'GitHub.svg', URL: 'https://www.github.com', type: 'gold' },
+    { imageKey: 'Replit', imagePath: 'Replit.png', URL: 'https://replit.com/', type: 'gold' },
+    { imageKey: 'Solana', imagePath: 'Solana.svg', URL: 'https://solana.com/', type: 'gold' },
+    { imageKey: 'Filecoin', imagePath: 'Filecoin.svg', URL: 'https://filecoin.io/', type: 'gold' },
+    { imageKey: 'Wolfram', imagePath: 'Wolfram.svg', URL: 'https://www.wolfram.com/', type: 'gold' },
     { imageKey: 'Echo3D', imagePath: 'echo3D.png', URL: 'https://www.echo3d.co/', type: 'gold' },
     { imageKey: 'Voiceflow', imagePath: 'voiceflow.png', URL: 'https://www.voiceflow.com/', type: 'gold' },
     { imageKey: 'FossUnited', imagePath: 'FOSSUnited.svg', URL: 'https://fossunited.org', type: 'silver' },
@@ -43,6 +48,7 @@ function DefineSponsorsList() {
         }
 
         if (tempList.length !== 0) {
+            listFull.push(<h3>{precedenceOrder[i]}</h3>);
             listFull.push(<div className="sponsorTier">{tempList}</div>);
             listFull.push(<DefineSeperator></DefineSeperator>);
         }
